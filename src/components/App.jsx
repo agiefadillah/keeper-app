a18458a4d369d75908711d94dbfce5d1c9581826
Agie Fadillah <arigierf@gmail.com>
2024-01-13 16:43:50 +0700

    🌳

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,17 @@ import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 
+import notes from '../notes';
+
 function App() {
   return (
     <div>
       <Header />
       <Footer />
 
-      <Note />
+      {notes.map((notesIsi) => (
+        <Note key={notesIsi.key} title={notesIsi.title} content={notesIsi.content} />
+      ))}
     </div>
   );
 }
